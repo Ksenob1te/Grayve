44cb0bd1077e9638a059bc053a9cd6e5590623f7
IvarysYT <skalin_i_i@mail.ru>
2023-12-25 18:37:34 +0300

    dict start implementation

--- static/include/dict.h
+++ static/include/dict.h
@@ -2,27 +2,63 @@
 #define GRAYVE_DICT_H
 #include <compare>
 #include <initializer_list>
+#include <utility>
+#include <functional>
+#include <memory>
 
-template<class Key, class T, class Compare = less<Key>,
-        class Allocator = allocator<pair<const Key, T>>>
+
+// ListIterator<T, false> -> iterator
+// ListIterator<T, true> -> const_iterator
+template<typename T, bool is_const>
+class DictIterator {
+
+private:
+    typedef std::conditional_t<is_const, const T, T> *node_ptr_t;
+    node_ptr_t currentPosition;
+
+    friend DictIterator<T, !is_const>;
+public:
+    using difference_type           = std::ptrdiff_t;
+    using value_type                = T;
+    using pointer                   = std::conditional_t<is_const, const T, T>*;
+    using reference                 = std::conditional_t<is_const, const T, T>&;
+    using iterator_category         = std::bidirectional_iterator_tag;
+
+    inline DictIterator(node_ptr_t currentPosition): currentPosition(currentPosition) {}
+
+    // copy constructor
+    template<bool other_const>
+    inline DictIterator(DictIterator<T, other_const>& other)
+        noexcept
+        requires (is_const >= other_const)
+        : DictIterator(other.currentPosition) {};
+
+    // move constructor
+    inline DictIterator(BidirectionalIterator&& iterator) noexcept
+    :DictIterator(iterator.currentPosition, iterator.begin, iterator.end) {}
+};
+
+
+template<class Key, class T, class Compare = std::less<Key>,
+        class Allocator = std::allocator<std::pair<const Key, T>>>
 class Dict {
-    using key_type               = Key;
-    using mapped_type            = T;
-    using value_type             = pair<const Key, T>;
-    using key_compare            = Compare;
-    using allocator_type         = Allocator;
-    using pointer                = typename allocator_traits<Allocator>::pointer;
-    using const_pointer          = typename allocator_traits<Allocator>::const_pointer;
-    using reference              = value_type&;
-    using const_reference        = const value_type&;
-    using size_type              = /* implementation-defined */;
-    using difference_type        = /* implementation-defined */;
-    using iterator               = /* implementation-defined */;
-    using const_iterator         = /* implementation-defined */;
-    using reverse_iterator       = std::reverse_iterator<iterator>;
-    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
-    using node_type              = /* unspecified */;
-    using insert_return_type     = __insert_return_type<iterator, node_type>;
+    using key_type                  = Key;
+    using mapped_type               = T;
+    using value_type                = std::pair<const Key, T>;
+    using key_compare               = Compare;
+    using allocator_type            = Allocator;
+    using pointer                   = typename std::allocator_traits<Allocator>::pointer;
+    using const_pointer             = typename std::allocator_traits<Allocator>::const_pointer;
+    using reference                 = value_type&;
+    using const_reference           = const value_type&;
+    using size_type                 = std::size_t;
+    using difference_type           = std::ptrdiff_t;
+    using iterator                  = /* implementation-defined */;
+    using const_iterator            = /* implementation-defined */;
+    using reverse_iterator          = std::reverse_iterator<iterator>;
+    using const_reverse_iterator    = std::reverse_iterator<const_iterator>;
+//    using node_type                 = /* unspecified */;
+    using insert_return_type        = __insert_return_type<iterator, node_type>;
 };
 
 #endif //GRAYVE_DICT_H
